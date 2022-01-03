a8681941639f5f59bc449c10f554acd8338cd17a
Angelo <angeloechaves@gmail.com>
2022-01-03 19:50:31 -0300

    Initial Commit

---
+++ ShowMeTheMusic/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ShowMeTheMusic
+//
+//  Created by Angelo Chaves on 2022-01-03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ShowMeTheMusic/ShowMeTheMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShowMeTheMusicApp.swift
+//  ShowMeTheMusic
+//
+//  Created by Angelo Chaves on 2022-01-03.
+//
+
+import SwiftUI
+
+@main
+struct ShowMeTheMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
